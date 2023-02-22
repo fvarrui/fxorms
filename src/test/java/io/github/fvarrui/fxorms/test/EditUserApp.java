f5abb3208eac68a7a8a27afe29203d3193abb962
fvarrui <4354486+fvarrui@users.noreply.github.com>
2023-02-22 09:36:20 +0000

    reduce sample

--- src/test/java/io/github/fvarrui/fxorms/test/EditUserApp.java
+++ src/test/java/io/github/fvarrui/fxorms/test/EditUserApp.java
@@ -1,15 +1,12 @@
 package io.github.fvarrui.fxorms.test;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.UUID;
 
 import io.github.fvarrui.fxorms.Form;
 import io.github.fvarrui.fxorms.FormBuilder;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -20,32 +17,26 @@ public class EditUserApp extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		
 		User user = new User(
-				"12345678Z", 
-				1200.0, 
+				12345, 
 				"Chuck Norris", 
 				LocalDateTime.now(), 
-				new Image("/images/trash-alt-solid.png"), 
 				new HashMap<String, Object>() {{
 					put("texto", "hola");
 					put("numero", 123);
 				}}, 
-				2.5, 
-				Arrays.asList("Reserva1", "Reserva2"), 
-				UUID.randomUUID(), 
-				"chuck", 
-				"cnorris@gmail.com", 
 				true
 		);
-				
 		
-		Form<User> form = new FormBuilder<>(User.class).build();
+		Form<User> form = new FormBuilder<>(User.class)
+				.registerFieldAdapter(null)
+				.build();
 		form.bind(user);
 		
 		BorderPane root = new BorderPane(form);
 
-		Scene scene = new Scene(root, 640, 480);
+		Scene scene = new Scene(root, 600, 400);
 		
-		primaryStage.setTitle("FXorms test :: Edit user");
+		primaryStage.setTitle("f(x)orms test");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
--- src/test/java/io/github/fvarrui/fxorms/test/User.java
+++ src/test/java/io/github/fvarrui/fxorms/test/User.java
@@ -1,112 +1,66 @@
 package io.github.fvarrui.fxorms.test;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
-import io.github.fvarrui.fxorms.annotations.Multiline;
 import io.github.fvarrui.fxorms.annotations.ReadOnly;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.ListProperty;
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.MapProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.ReadOnlyBooleanWrapper;
-import javafx.beans.property.ReadOnlyStringWrapper;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleMapProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
-import javafx.scene.image.Image;
 
 public class User {
 
 	@ReadOnly
-	private StringProperty id = new SimpleStringProperty();
+	private IntegerProperty id = new SimpleIntegerProperty();
 
-	private DoubleProperty coins = new SimpleDoubleProperty();
-
-	@Multiline
-	private StringProperty displayName = new SimpleStringProperty();
+	private StringProperty name = new SimpleStringProperty();
 
 	@ReadOnly
 	private ObjectProperty<LocalDateTime> lastLogin = new SimpleObjectProperty<>();
 
-	private ObjectProperty<Image> photo = new SimpleObjectProperty<>();
-
 	private MapProperty<String, Object> preferences = new SimpleMapProperty<>(FXCollections.observableHashMap());
-
-	private DoubleProperty rating = new SimpleDoubleProperty();
-
-	private ListProperty<String> reserved = new SimpleListProperty<>(FXCollections.observableArrayList());
-
-	private ObjectProperty<UUID> shared = new SimpleObjectProperty<>();
-
-	private ReadOnlyStringWrapper name = new ReadOnlyStringWrapper();
-
-	private ReadOnlyStringWrapper email = new ReadOnlyStringWrapper();
 
 	private ReadOnlyBooleanWrapper enabled = new ReadOnlyBooleanWrapper();
 
-	public User(String id, Double coins, String displayName,
-			LocalDateTime lastLogin, Image photo,
-			Map<String, Object> preferences, Double rating, List<String> reserved,
-			UUID shared, String name, String email,
-			Boolean enabled) {
+	public User(int id, String name, LocalDateTime lastLogin, Map<String, Object> preferences, Boolean enabled) {
 		super();
 		setId(id);
-		setCoins(coins);
-		setDisplayName(displayName);
+		setName(name);
 		setLastLogin(lastLogin);
-		setPhoto(photo);
 		getPreferences().putAll(preferences);
-		setRating(rating);
-		getReserved().setAll(reserved);
-		setShared(shared);		
-		this.name.set(name);
-		this.email.set(email);
 		this.enabled.set(true);
 	}
 
-	public final StringProperty idProperty() {
+	public final IntegerProperty idProperty() {
 		return this.id;
 	}
 
-	public final String getId() {
+	public final int getId() {
 		return this.idProperty().get();
 	}
 
-	public final void setId(final String id) {
+	public final void setId(final int id) {
 		this.idProperty().set(id);
 	}
 
-	public final DoubleProperty coinsProperty() {
-		return this.coins;
+	public final StringProperty nameProperty() {
+		return this.name;
 	}
 
-	public final double getCoins() {
-		return this.coinsProperty().get();
+	public final String getName() {
+		return this.nameProperty().get();
 	}
 
-	public final void setCoins(final double coins) {
-		this.coinsProperty().set(coins);
-	}
-
-	public final StringProperty displayNameProperty() {
-		return this.displayName;
-	}
-
-	public final String getDisplayName() {
-		return this.displayNameProperty().get();
-	}
-
-	public final void setDisplayName(final String displayName) {
-		this.displayNameProperty().set(displayName);
+	public final void setName(final String name) {
+		this.nameProperty().set(name);
 	}
 
 	public final ObjectProperty<LocalDateTime> lastLoginProperty() {
@@ -121,18 +75,6 @@ public class User {
 		this.lastLoginProperty().set(lastLogin);
 	}
 
-	public final ObjectProperty<Image> photoProperty() {
-		return this.photo;
-	}
-
-	public final Image getPhoto() {
-		return this.photoProperty().get();
-	}
-
-	public final void setPhoto(final Image photo) {
-		this.photoProperty().set(photo);
-	}
-
 	public final MapProperty<String, Object> preferencesProperty() {
 		return this.preferences;
 	}
@@ -143,58 +85,6 @@ public class User {
 
 	public final void setPreferences(final ObservableMap<String, Object> preferences) {
 		this.preferencesProperty().set(preferences);
-	}
-
-	public final DoubleProperty ratingProperty() {
-		return this.rating;
-	}
-
-	public final double getRating() {
-		return this.ratingProperty().get();
-	}
-
-	public final void setRating(final double rating) {
-		this.ratingProperty().set(rating);
-	}
-
-	public final ListProperty<String> reservedProperty() {
-		return this.reserved;
-	}
-
-	public final ObservableList<String> getReserved() {
-		return this.reservedProperty().get();
-	}
-
-	public final void setReserved(final ObservableList<String> reserved) {
-		this.reservedProperty().set(reserved);
-	}
-
-	public final ObjectProperty<UUID> sharedProperty() {
-		return this.shared;
-	}
-
-	public final UUID getShared() {
-		return this.sharedProperty().get();
-	}
-
-	public final void setShared(final UUID shared) {
-		this.sharedProperty().set(shared);
-	}
-
-	public final javafx.beans.property.ReadOnlyStringProperty nameProperty() {
-		return this.name.getReadOnlyProperty();
-	}
-
-	public final String getName() {
-		return this.nameProperty().get();
-	}
-
-	public final javafx.beans.property.ReadOnlyStringProperty emailProperty() {
-		return this.email.getReadOnlyProperty();
-	}
-
-	public final String getEmail() {
-		return this.emailProperty().get();
 	}
 
 	public final javafx.beans.property.ReadOnlyBooleanProperty enabledProperty() {
